11f778846d215c79fb93717fc08f3f89e85097e9
Hiren02 <hirenchavda0201@gmail.com>
2022-08-27 18:55:03 -0700

    first commit

---
+++ HTML/tut53.js
@@ -0,0 +1,17 @@
+console.log("This is Tutorial");
+
+function greet(name){
+    console.log(name+ "is a good person");
+}
+                             
+let name ="Vasu";
+let name1="Buttki";
+let name2="Bhuri";
+let name3="Balo";
+                                                                            
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
+
+
---
+++ HTML/tut67.js/index.js
@@ -0,0 +1,15 @@
+// const average = require("./mod");
+// console.log(average([3,4]));
+const mod = require("./mod");
+
+mod={
+    
+    avg : average,
+    name: "Vasu",
+    repo:"CWH"
+} 
+
+console.log(mod.avg([3,4]));
+console.log("This is index")
+
+
